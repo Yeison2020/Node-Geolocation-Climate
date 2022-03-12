0004f399d820183da1cf4c6a31c594154d0896e9
Yeison <enmanuelquiterios@gmail.com>
2022-03-12 12:43:13 -0500

    Displaying console Options

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const { leerInput, inquirerMenu } = require("./helpers/inquirer.js");
+const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer.js");
 
 const main = async () => {
   let opt;
   do {
-    opt = inquirerMenu;
+    opt = await inquirerMenu();
+    console.log({ opt });
+    await pausa();
   } while (opt !== 0);
 };
 
