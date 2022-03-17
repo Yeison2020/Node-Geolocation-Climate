2fbfbb8852e550796e0001bd65be30f31aa5f081
Yeison <enmanuelquiterios@gmail.com>
2022-03-17 13:19:24 -0400

    Working on how I will display the data

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ const main = async () => {
   let opt;
   do {
     opt = await inquirerMenu();
+    switch (opt) {
+      case 1:
+        // Show message
+
+        // Search places
+
+        // Chose the place
+
+        // Weather data
+
+        // Show those results
+        console.log(`\n Inofrmation de la ciudad\n`.green);
+
+        break;
+    }
     console.log({ opt });
 
     if (opt !== 0) await pausa();
