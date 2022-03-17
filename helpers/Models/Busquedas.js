bce20a559e9a798c45e4da78e40fd04d3c17c7e6
Yeison <enmanuelquiterios@gmail.com>
2022-03-17 13:16:05 -0400

    Adding

---
+++ helpers/Models/Busquedas.js
@@ -0,0 +1,16 @@
+class Busquedas {
+  historial = ["Tegucigalpa", "Madrid", "San Jose"];
+  constructor() {
+    // TODO  LEER DB si existes
+  }
+
+  async ciudad(lugar = "") {
+    // HTTP Request
+
+    console.log(lugar);
+
+    return []; // Return an  array that matches the array of names availables
+  }
+}
+
+module.exports = Busquedas;
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer.js");
+const Busquedas = require("./helpers/Models/Busquedas.js");
 
 const main = async () => {
+  // Here I'm importing my class to used It's methods
+
+  const busquedas = new Busquedas();
   let opt;
   do {
     opt = await inquirerMenu();
