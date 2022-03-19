241939579860db35c1ed336f9ed317cd6ab93cb5
Yeison <enmanuelquiterios@gmail.com>
2022-03-19 11:45:10 -0400

    Adding values

--- helpers/Models/Busquedas.js
+++ helpers/Models/Busquedas.js
@@ -18,10 +18,10 @@ class Busquedas {
     // HTTP Request
 
     console.log("Ciudad:", lugar);
-
-    const resp = await axios.get(
-      "https://api.mapbox.com/geocoding/v5/mapbox.places/republica%20dominicana.json?proximity=ip&language=es&access_token=pk.eyJ1IjoieWVpc29uNjk1IiwiYSI6ImNsMHkwNzF6ODF3OTAzY3A0MTYwcnkwYm8ifQ.8KheJHm4b8znESJHQnbpqw"
-    );
+    const instance = axios.create({
+      baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json?proximity=ip&language=es&access_token=pk.eyJ1IjoieWVpc29uNjk1IiwiYSI6ImNsMHkwNzF6ODF3OTAzY3A0MTYwcnkwYm8ifQ.8KheJHm4b8znESJHQnbpqw`,
+    });
+    const resp = await axios.get("");
     console.log(resp.data);
 
     return [resp.data.per_page]; // Return an  array that matches the array of names availables
