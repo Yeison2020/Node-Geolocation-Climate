457eb5a74fee78089e09c6b83cd6c6d6fb2c0ca2
Yeison <enmanuelquiterios@gmail.com>
2022-03-19 11:37:18 -0400

    Adding api

--- helpers/Models/Busquedas.js
+++ helpers/Models/Busquedas.js
@@ -19,8 +19,10 @@ class Busquedas {
 
     console.log("Ciudad:", lugar);
 
-    const resp = await axios.get("https://reqres.in/api/users?page=2");
-    console.log(resp.data.per_page);
+    const resp = await axios.get(
+      "https://api.mapbox.com/geocoding/v5/mapbox.places/republica%20dominicana.json?proximity=ip&language=es&access_token=pk.eyJ1IjoieWVpc29uNjk1IiwiYSI6ImNsMHkwNzF6ODF3OTAzY3A0MTYwcnkwYm8ifQ.8KheJHm4b8znESJHQnbpqw"
+    );
+    console.log(resp.data);
 
     return [resp.data.per_page]; // Return an  array that matches the array of names availables
   }
